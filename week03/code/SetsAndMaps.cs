059a2874b1b2d797d56a09dedea5d816991e2f80
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2024-11-23 14:00:13 -0800

    Problems 1-5 completed

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,26 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        HashSet<string> seen = new HashSet<string>();
+        List<string> result = new List<string>();
+
+        foreach (var word in words)
+        {
+            string reverse = new string(word.Reverse().ToArray());
+
+            if (word[0] == word[1]) continue;
+
+            if (seen.Contains(reverse))
+            {
+                result.Add($"{reverse} & {word}");
+            }
+            else
+            {
+                seen.Add(word);
+            }
+        }
+
+        return result.ToArray();
     }
 
     /// <summary>
@@ -39,14 +57,30 @@ public static class SetsAndMaps
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
+
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+
+            if (fields.Length < 4) continue;
+
+            var degree = fields[3].Trim();
+
+            if (string.IsNullOrEmpty(degree)) continue;
+
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
     }
+
 
     /// <summary>
     /// Determine if 'word1' and 'word2' are anagrams.  An anagram
@@ -66,9 +100,34 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        if (word1.Length != word2.Length)
+            return false;
+
+        var charCount1 = new Dictionary<char, int>();
+        var charCount2 = new Dictionary<char, int>();
+
+        foreach (char c in word1)
+        {
+            if (charCount1.ContainsKey(c))
+                charCount1[c]++;
+            else
+                charCount1[c] = 1;
+        }
+
+        foreach (char c in word2)
+        {
+            if (charCount2.ContainsKey(c))
+                charCount2[c]++;
+            else
+                charCount2[c] = 1;
+        }
+
+        return charCount1.Count == charCount2.Count && !charCount1.Except(charCount2).Any();
     }
+
 
     /// <summary>
     /// This function will read JSON (Javascript Object Notation) data from the 
@@ -87,6 +146,7 @@ public static class SetsAndMaps
     public static string[] EarthquakeDailySummary()
     {
         const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
         using var client = new HttpClient();
         using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
         using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
@@ -96,11 +156,12 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        var descriptions = featureCollection.Features
+            .Where(feature => feature.Properties.Mag.HasValue && !string.IsNullOrWhiteSpace(feature.Properties.Place))
+            .Select(feature => $"{feature.Properties.Place} - Mag {feature.Properties.Mag:F2}")
+            .ToArray();
+
+        return descriptions;
     }
+
 }
